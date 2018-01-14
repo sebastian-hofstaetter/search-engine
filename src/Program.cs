0e10d3af78048f832199da742c3800005b239ddd
Sebastian Hofstaetter <sebastian-hofstaetter@users.noreply.github.com>
2018-01-14 15:29:23 +0100

    Empty .net core project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SearchEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
